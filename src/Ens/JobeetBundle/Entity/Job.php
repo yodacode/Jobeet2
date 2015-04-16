249cc43caad346811ad49e3fe725f05b878c7cc7
Benjamin de Vaublanc <benjamin.devaublanc@popsiit.net>
2015-04-16 15:19:21 +0200

    Day 5 Routing

--- src/Ens/JobeetBundle/Entity/Job.php
+++ src/Ens/JobeetBundle/Entity/Job.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Ens\JobeetBundle\Entity;
+use Ens\JobeetBundle\Utils\Jobeet as Jobeet;
 
 use Doctrine\ORM\Mapping as ORM;
 
@@ -489,5 +490,20 @@ class Job
     public function setUpdatedAtValue()
     {
         $this->updated_at = new \DateTime();
+    }
+
+    public function getCompanySlug()
+    {
+        return Jobeet::slugify($this->getCompany());
+    }
+ 
+    public function getPositionSlug()
+    {
+        return Jobeet::slugify($this->getPosition());
+    }
+ 
+    public function getLocationSlug()
+    {
+        return Jobeet::slugify($this->getLocation());
     }
 }
